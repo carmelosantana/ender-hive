5f89292c79a29557b33b345d9c8178b8a670fb14
Carmelo Santana <me@carmelosantana.com>
2022-08-19 17:45:49 -0400

    Fixed stop command
    Change to $id from $post_name
    Add comments

--- src/Host/Screen.php
+++ src/Host/Screen.php
@@ -6,6 +6,7 @@ namespace CarmeloSantana\EnderHive\Host;
 
 use CarmeloSantana\EnderHive\Command as Command;
 use CarmeloSantana\EnderHive\Instance as Instance;
+use CarmeloSantana\EnderHive\Server;
 
 class Screen implements Host
 {
@@ -13,28 +14,35 @@ class Screen implements Host
 
     private string $start = 'start.sh';
 
-    public function __construct(private string $instance)
+    /**
+     * Returns server instance. Requires post ID.
+     *
+     * @param  int $id
+     * @return void
+     */
+    public function __construct(private int $id)
     {
-        $path = Instance::getPath($this->instance);
+        $this->server = new Server($this->id);
 
-        chdir($path);
+        chdir($this->server->getPath());
     }
 
+    /**
+     * Starts server with screen name of $id.
+     *
+     * @return bool
+     */
     public function start(): bool
     {
-        $command = [$this->host, '-dmS', $this->instance, 'bash', $this->start];
+        $command = [$this->host, '-dmS', $this->id, 'bash', $this->start];
 
-        $output = Command::exec($command);
-
-        return true;
+        return Command::exec($command);
     }
 
     public function stop(): bool
     {
-        $command = [$this->host, '-S', $this->instance, '-p', '0', '-X', 'stop^M'];
+        $command = [$this->host, '-S', $this->id, '-p', '0', '-X', 'stuff', 'stop^M'];
 
-        $output = Command::exec($command);
-
-        return true;
+        return Command::exec($command);
     }
 }
