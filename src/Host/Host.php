7e495cc7ed7943557cc25bd2955c9399d3ff5cde
Carmelo Santana <me@carmelosantana.com>
2022-08-20 10:52:14 -0400

    Change from interface to abstract class
    Move exec() from Command class

--- src/Host/Host.php
+++ src/Host/Host.php
@@ -4,9 +4,37 @@ declare(strict_types=1);
 
 namespace CarmeloSantana\EnderHive\Host;
 
-interface Host
-{
-    public function start(): bool;
+use CarmeloSantana\EnderHive\Status;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
-    public function stop(): bool;
+abstract class Host
+{
+    public function exec(array $command): int
+    {
+        $process = new Process($command);
+
+        try {
+            $process->mustRun();
+
+            $log = $process->getOutput();
+            $status = Status::ACCEPTED;
+        } catch (ProcessFailedException $exception) {
+            $log = $exception->getMessage();
+            $status = Status::INTERNAL_SERVER_ERROR;
+        }
+
+        ray($command)->label('Process() $command');
+        ray($log)->label('Process() $log');
+
+        return $status;
+    }
+
+    public function start(): int
+    {
+    }
+
+    public function stop(): int
+    {
+    }
 }
