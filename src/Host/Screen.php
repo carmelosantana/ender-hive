dafbb99ce49080135b33868ce89f8604e4468645
Carmelo Santana <me@carmelosantana.com>
2022-08-20 20:20:56 -0400

    Cleanup use classes

--- src/Host/Screen.php
+++ src/Host/Screen.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace CarmeloSantana\EnderHive\Host;
 
-use CarmeloSantana\EnderHive\Command as Command;
-use CarmeloSantana\EnderHive\Instance as Instance;
+use CarmeloSantana\EnderHive\Command;
+use CarmeloSantana\EnderHive\Instance;
 use CarmeloSantana\EnderHive\Server;
 
 class Screen extends Host
