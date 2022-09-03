a03cc7d625a24202e48c7965c1f624c645e8b4ea
Carmelo Santana <me@carmelosantana.com>
2022-09-02 20:22:23 -0400

    Moved exec() to Screen.php
    Convert to interface
    Add comments

--- src/Host/Host.php
+++ src/Host/Host.php
@@ -4,37 +4,70 @@ declare(strict_types=1);
 
 namespace CarmeloSantana\EnderHive\Host;
 
-use CarmeloSantana\EnderHive\Status;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
+interface Host
+{    
+    /**
+     * Host wrapper for the server binary or the host container.
+     *
+     * @param  mixed $post_id Instance post ID of the server.
+     * @return void
+     */
+    public function __construct(int $post_id);
+    
+    /**
+     * Retrieve IPv4 port.
+     *
+     * @return int
+     */
+    public function getPortIp4(): int;
+    
+    /**
+     * Retrieve IPv6 port.
+     *
+     * @return int
+     */
+    public function getPortIp6(): int;
+    
+    /**
+     * Install and start server.
+     *
+     * @return int Status constant.
+     */
+    public function install(): int;
+    
+    /**
+     * Checks if server is running.
+     *
+     * @return bool True if server is running, false otherwise.
+     */
+    public function isRunning(): bool;
+    
+    /**
+     * Starts the server.
+     *
+     * @return int Status constant.
+     */
+    public function start(): int;
 
-abstract class Host
-{
-    public function exec(array $command): int
-    {
-        $process = new Process($command);
+    /**
+     * Stops the server.
+     *
+     * @return int Status constant.
+     */
+    public function stop(): int;
 
-        try {
-            $process->mustRun();
-
-            $log = $process->getOutput();
-            $status = Status::ACCEPTED;
-        } catch (ProcessFailedException $exception) {
-            $log = $exception->getMessage();
-            $status = Status::INTERNAL_SERVER_ERROR;
-        }
-
-        ray($command)->label('Process() $command');
-        ray($log)->label('Process() $log');
-
-        return $status;
-    }
-
-    public function start(): int
-    {
-    }
-
-    public function stop(): int
-    {
-    }
+    /**
+     * Waits till the server is fully stopped before returning.
+     *
+     * @return int Status constant.
+     */
+    public function stopWait(): int;
+    
+    /**
+     * Performs all processes necessary to update the server status.
+     * Hydrates the host with the latest status.
+     *
+     * @return void
+     */
+    public function updateStatus(): void;
 }
