c4235fc3afce6b97575eaf8e2315f2335633b93e
Carmelo Santana <me@carmelosantana.com>
2022-09-05 13:25:00 -0400

    Move newFiles() from Installer.php
    Move initServerProperties() to PocketMineMP/Server.php
    Update getPath arguments
    Add comments

--- src/Host/Base.php
+++ src/Host/Base.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CarmeloSantana\EnderHive\Host;
 
-use CarmeloSantana\EnderHive\Config\Defaults;
 use CarmeloSantana\EnderHive\Host\Status;
 use CarmeloSantana\EnderHive\Tools\Utils;
 
@@ -28,18 +27,55 @@ abstract class Base implements Host
     }
 
     /**
+     * Create new files with default values.
+     *
+     *  'plugin_list_yml' => [
+     *      'filename' => 'plugin_list',
+     *      'extension' => 'yml',
+     *      'callback' => [__CLASS__, 'getAsset'],
+     *  ],
+     *
+     * @param  mixed $files
+     * @return void
+     */
+    public function newFiles(array $files): void
+    {
+        foreach ($files as $file) {
+            // Build file path.
+            $filename = $file['filename'];
+            if (isset($file['extension'])) {
+                $filename .= '.' . $file['extension'];
+            }
+            $file_path = $this->getPath($filename);
+
+            switch ($filename) {
+                case 'server.properties':
+                    $file['content'] = call_user_func($file['callback'], $this->post_id);
+                    break;
+
+                default:
+                    // If callback not provide an empty string for the file content.
+                    $file['content'] = (isset($file['callback']) and is_callable($file['callback'])) ? call_user_func($file['callback'], $filename) : '';
+                    break;
+            }
+
+            file_put_contents($file_path, $file['content']);
+        }
+    }
+
+    /**
      * Get instance path but with instance ID.
      *
      * @param  array $file [filename => extension]
      * @return string
      */
-    public function getPath(array $file = []): string
+    public function getPath(array|string $file = []): string
     {
         return Server::getInstancePath($this->post_id, $file);
     }
-    
+
     /**
-     * Returns IPv4 port from $server_properties.
+     * Returns IPv4 port from $this->server_properties.
      *
      * @return int
      */
@@ -48,11 +84,16 @@ abstract class Base implements Host
         return (int) $this->server_properties['server-port'];
     }
 
+    /**
+     * Returns IPv6 port from $this->server_properties.
+     *
+     * @return int
+     */
     public function getPortIp6(): int
     {
         return $this->isIp6Enabled() ? (int) $this->server_properties['server-portv6'] : 0;
     }
-    
+
     /**
      * Returns status variable.
      *
@@ -62,7 +103,7 @@ abstract class Base implements Host
     {
         return $this->status;
     }
-    
+
     /**
      * Initialize components needed for the host.
      * Best practice to update status after this.
@@ -75,7 +116,7 @@ abstract class Base implements Host
         $this->initServerProperties();
         $this->updateStatus();
     }
-    
+
     /**
      * Retrieves post data and sets it to the class.
      *
@@ -85,33 +126,14 @@ abstract class Base implements Host
     {
         $this->post = get_post($this->post_id);
     }
-    
+
     /**
-     * Check if IPv6 is enabled via $server_properties.
+     * Check if IPv6 is enabled via $this->server_properties.
      *
      * @return bool
      */
     public function isIp6Enabled(): bool
     {
         return Utils::isEnabled($this->server_properties['enable-ipv6']);
-    }
-    
-    /**
-     * Populates $server_properties with the server.properties options.
-     *
-     * @return void
-     */
-    public function initServerProperties(): void
-    {
-        $this->server_properties = [];
-
-        foreach (Defaults::serverProperties() as $property => $default) {
-            // Property is a comment.
-            if (is_int($property)) {
-                continue;
-            }
-
-            $this->server_properties[$property] = carbon_get_post_meta($this->post_id, $property);
-        }
     }
 }
