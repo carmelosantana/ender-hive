843614d0ea82c9e59175944d476031019f13575e
Carmelo Santana <me@carmelosantana.com>
2022-09-05 13:01:26 -0400

    Update comments
    Add getStatus()
    Add isIp6Enabled()
    Add restart()

--- src/Host/Host.php
+++ src/Host/Host.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace CarmeloSantana\EnderHive\Host;
 
 interface Host
-{    
+{
     /**
      * Host wrapper for the server binary or the host container.
      *
@@ -13,14 +13,14 @@ interface Host
      * @return void
      */
     public function __construct(int $post_id);
-    
+
     /**
      * Retrieve IPv4 port.
      *
      * @return int
      */
     public function getPortIp4(): int;
-    
+
     /**
      * Retrieve IPv6 port.
      *
@@ -29,40 +29,61 @@ interface Host
     public function getPortIp6(): int;
     
     /**
+     * Retrieves current status.
+     *
+     * @return int
+     */
+    public function getStatus(): int;
+
+    /**
      * Install and start server.
      *
-     * @return int Status constant.
+     * @return int Status code.
      */
     public function install(): int;
-    
+
+    /**
+     * Checks if IPv6 is enabled.
+     *
+     * @return int Status code.
+     */
+    public function isIp6Enabled(): bool;
+
     /**
      * Checks if server is running.
      *
      * @return bool True if server is running, false otherwise.
      */
     public function isRunning(): bool;
-    
+
     /**
-     * Starts the server.
+     * Restart server.
      *
-     * @return int Status constant.
+     * @return int Status code.
+     */
+    public function restart(): int;
+
+    /**
+     * Start server.
+     *
+     * @return int Status code.
      */
     public function start(): int;
 
     /**
-     * Stops the server.
+     * Stop server.
      *
-     * @return int Status constant.
+     * @return int Status code.
      */
     public function stop(): int;
 
     /**
      * Waits till the server is fully stopped before returning.
      *
-     * @return int Status constant.
+     * @return int Status code.
      */
     public function stopWait(): int;
-    
+
     /**
      * Performs all processes necessary to update the server status.
      * Hydrates the host with the latest status.
