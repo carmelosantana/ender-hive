cdef9b5a7c93e5d32c765b8a39f46101a862c965
Carmelo Santana <me@carmelosantana.com>
2022-09-11 09:12:12 -0400

    Removing unused methods from Base abstract

--- src/API/Base.php
+++ src/API/Base.php
@@ -20,39 +20,6 @@ abstract class Base
 
     public const PERMISSION_WRITE = 'edit_posts';
 
-    // Here initialize our namespace and resource name.
-    public function __construct()
-    {
-        $this->resource_name = '';
-    }
-
-    // Register our routes.
-    public function register_routes()
-    {
-        register_rest_route($this->namespace, '/' . $this->resource_name, [
-            // Here we register the readable endpoint for collections.
-            [
-                'methods'   => 'GET',
-                'callback'  => [$this, 'get_items'],
-                'permission_callback' => [$this, 'get_items_permissions_check'],
-            ],
-            // Register our schema callback.
-            'schema' => [
-                $this, 'get_item_schema'
-            ],
-        ]);
-        register_rest_route($this->namespace, '/' . $this->resource_name . '/(?P<id>[\d]+)', array(
-            // Notice how we are registering multiple endpoints the 'schema' equates to an OPTIONS request.
-            [
-                'methods'   => 'GET',
-                'callback'  => [$this, 'get_item'],
-                'permission_callback' => [$this, 'get_item_permissions_check'],
-            ],
-            // Register our schema callback.
-            'schema' => [$this, 'get_item_schema'],
-        ));
-    }
-
     /**
      * Check permissions for the posts.
      *
@@ -64,35 +31,6 @@ abstract class Base
             return new WP_Error('rest_forbidden', esc_html__('You cannot access the instance resource.'), array('status' => $this->authorization_status_code()));
         }
         return true;
-    }
-
-    /**
-     * Grabs the five most recent posts and outputs them as a rest response.
-     *
-     * @param WP_REST_Request $request Current request.
-     */
-    public function get_items($request)
-    {
-        $args = [
-            'post_type' => 'instance',
-            'posts_per_page' => -1,
-            'author' => get_current_user_id(),
-        ];
-        $posts = get_posts($args);
-
-        $data = [];
-
-        if (empty($posts)) {
-            return rest_ensure_response($data);
-        }
-
-        foreach ($posts as $post) {
-            $response = $this->prepare_item_for_response($post, $request);
-            $data[] = $this->prepare_response_for_collection($response);
-        }
-
-        // Return all of our comment response data.
-        return rest_ensure_response($data);
     }
 
     /**
@@ -109,31 +47,11 @@ abstract class Base
     }
 
     /**
-     * Grabs the five most recent posts and outputs them as a rest response.
-     *
-     * @param WP_REST_Request $request Current request.
-     */
-    public function get_item($request)
-    {
-        $id = (int) $request['id'];
-        $post = get_post($id);
-
-        if (empty($post)) {
-            return rest_ensure_response([]);
-        }
-
-        $response = $this->prepare_item_for_response($post, $request);
-
-        // Return all of our post response data.
-        return $response;
-    }
-
-    /**
      * Matches the post data to the schema we want.
      *
      * @param WP_Post $post The comment object whose response is being prepared.
      */
-    public function prepare_item_for_response($post, $request)
+    public function prepare_item_for_response(\WP_REST_Request $request)
     {
         $post_data = [];
 
@@ -186,7 +104,7 @@ abstract class Base
      *
      * @return array The sample schema for a post
      */
-    public function get_item_schema()
+    public function get_item_schema(\WP_REST_Request $request)
     {
         if (isset($this->schema)) {
             // Since WordPress 5.3, the schema can be cached in the $schema property.
